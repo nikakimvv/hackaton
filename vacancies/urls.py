58195291c79d7469fcf67c2bdf04eaf8016c5eaa
Nikita Akimov <nikitaamogus@gmail.com>
2025-06-13 15:49:26 +0300

    Version 0.1

--- vacancies/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.job_search_view, name='job_search'),
-    path('statistics/', views.statistics_view, name='statistics'),
-]
